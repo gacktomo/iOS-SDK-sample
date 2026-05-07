168974b0a1c0983eccd990f15c2cbbfc6cf3fd93
tmatsuo <gacktomo@gmail.com>
2026-05-07 16:39:06 +0900

    Initial Commit

---
+++ demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by omatsu on 2026/05/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by omatsu on 2026/05/07.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
